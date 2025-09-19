81a00a2729944d7f8daf2292a3065fc5dd74bd64
macbook <shohruhabdullayev17@icloud.com>
2025-09-19 19:24:42 +0500

    shohruh.py

--- shohruh.py
+++ shohruh.py
@@ -1,11 +1,11 @@
-
+import datetime
 def ism_familiya():
     birinchi = input("Ismingizni kiriting: ").upper()
     ikkinchi = input("Familyangizni kiriting: ").upper()
     tugilgan_yil = int(input("Tug‘ilgan yilingizni kiriting: ").strip())
-    hozirgi_yil = int(input("Hozirgi yilni kiriting: ").strip())
+    hozirgi_yil = datetime.datetime.now().year - tugilgan_yil
 
-    yosh = hozirgi_yil - tugilgan_yil
+    yosh = hozirgi_yil
     print(f"Salom {birinchi} {ikkinchi}, sizning yoshingiz {yosh} da.")
 
 if __name__ == "__main__":
